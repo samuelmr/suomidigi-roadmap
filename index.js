42d71ba59765b4e01e6484462a63a858e996ac1f
Samuel Rinnetmäki <samuel@iki.fi>
2019-12-11 17:29:49 +0200

    serve static files from cache

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
+const path = require('path');
 const {Package} = require('datapackage');
 const express = require('express');
 const config = require('./config.js');
 const sheets2package = require('./lib/sheets2package');
 
-
-
-async function createDataPackage(descriptor) {
-  const package = await Package.load(descriptor);
-  if (!package.valid) {
-    console.error('Invalid contents for data package!');
-  }
-  for (var i=0; i< package.descriptor.resources.length; i++) {
-    let resource = package.descriptor.resources[i];
-    await package.getResource(resource.name).checkRelations();
-  }
-  await package.save(config.packageFile);
-  console.log("Created " + config.packageFile);
-}
-
-
 const app = express();
+
+// serve cahced roadmap.json and roadmap.vg.json from a folder
+const dataDir = path.join(__dirname, 'data');
+app.use('/data', express.static(dataDir));
+console.log('Serving static files from %s', dataDir);
+
+// the root URL for the service returns dynamically created
+// latest version of the roadmap
+app.get('*', function (req, res) {
+  console.log(req);
+});
 
 app.get('/', function (req, res) {
   // console.log('Created index ' + config.dashboardIndex);
@@ -36,7 +32,7 @@ app.get('/', function (req, res) {
   });
 });
 
-let server = app.listen(config.defaultPort, function () {
+let server = app.listen(config.servicePort, function () {
   let host = server.address().address;
   let port = server.address().port;
   if (!host || (host === '::')) {
